c3e9ee6ab171675c9d812ab01b44af7ae8efef60
miki <mikigazdiev@yandex.ru>
2025-11-14 20:12:33 +0300

    fixed brain-prime

--- src/games/Prime.php
+++ src/games/Prime.php
@@ -10,7 +10,7 @@ function getQuestionAndAnswer() : array
 {
 	$question = rand(1, 100);
 	$correctAnswer = isPrime($question) ? 'yes' : 'no';
-	return [$question, $correctAnswer];
+	return [$question, (string)$correctAnswer];
 }
 
 function isPrime(int $number) : bool
