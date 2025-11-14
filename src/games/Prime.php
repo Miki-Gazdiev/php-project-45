90afef6a01a398e2aee333d9f40a7d504da31c71
miki <mikigazdiev@yandex.ru>
2025-11-14 21:11:36 +0300

    fixed Prime.php

--- src/games/Prime.php
+++ src/games/Prime.php
@@ -10,7 +10,7 @@ function getQuestionAndAnswer(): array
 {
     $question = rand(1, 100);
     $correctAnswer = isPrime($question) ? 'yes' : 'no';
-    return [(string)$question, (string)$correctAnswer];
+    return [(string)$question, $correctAnswer];
 }
 
 function isPrime(int $number): bool
